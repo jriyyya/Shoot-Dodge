2dc447396e286383c33a56db2a868db9d5552841
jriyyya <jriya.shi@gmail.com>
2023-03-12 11:46:52 +0530

    Added time elapsed

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 import time
 import random
+pygame.font.init()
+
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
+FONT = pygame.font.SysFont("comicsans", 30)
 BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 40
@@ -13,8 +16,11 @@ PLAYER_HEIGHT = 60
 
 PLAYER_VEL = 5
 
-def draw(player):
+def draw(player, elapsed_time):
     WIN.blit(BG, (0, 0))
+
+    time_text = FONT.render(f"TIME: {round(elapsed_time)}s", 1, "white")
+    WIN.blit(time_text, (10,10))
 
     pygame.draw.rect(WIN, (255, 255, 255), player)
     pygame.display.update()
@@ -26,9 +32,12 @@ def main():
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
 
     while run:
         clock.tick(60)
+        elapsed_time = time.time() - start_time
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -36,12 +45,12 @@ def main():
 
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT]:
+        if keys[pygame.K_LEFT] and player.x -PLAYER_VEL >= 0:
             player.x -= PLAYER_VEL
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
 
-        draw(player)
+        draw(player, elapsed_time)
     pygame.quit()
 
 if __name__ == "__main__":
