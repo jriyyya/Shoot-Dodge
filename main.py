c719dff8930d3cf950dba1ec80918889f3bc22e7
jriyyya <jriya.shi@gmail.com>
2023-03-12 11:57:33 +0530

    Added stars

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ PLAYER_HEIGHT = 60
 
 PLAYER_VEL = 5
 
-def draw(player, elapsed_time):
+STAR_WIDTH = 10
+STAR_HEIGHT = 20
+STAR_VEL = 3
+
+def draw(player, elapsed_time, stars):
     WIN.blit(BG, (0, 0))
 
     time_text = FONT.render(f"TIME: {round(elapsed_time)}s", 1, "white")
@@ -25,6 +29,8 @@ def draw(player, elapsed_time):
     pygame.draw.rect(WIN, (255, 255, 255), player)
     pygame.display.update()
 
+    for star in stars:
+        pygame.draw.rect(WIN, 'red', star)
 
 def main():
     run = True
@@ -35,9 +41,25 @@ def main():
     start_time = time.time()
     elapsed_time = 0
 
+    star_add_increment = 2000
+    star_count = 0
+    stars = []
+
+    hit = False
+
     while run:
-        clock.tick(60)
+        star_count += clock.tick(60)
         elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -50,6 +72,17 @@ def main():
         if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
             player.x += PLAYER_VEL
 
+        for star in stars[:]:
+            star.y += STAR_VEL
+            if star.y > HEIGHT:
+                stars.remove(star)
+            elif star.y + star.height >= player.y and star.colliderect(player):
+                stars.remove(star)
+                hit = True
+                break
+
+
+                
         draw(player, elapsed_time)
     pygame.quit()
 
