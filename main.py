98f032bb3a5d9f6ebd24708eef718460b3feccb7
jriyyya <jriya.shi@gmail.com>
2023-03-12 11:42:14 +0530

    Player movement

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 
+PLAYER_VEL = 5
+
 def draw(player):
     WIN.blit(BG, (0, 0))
 
@@ -23,11 +25,21 @@ def main():
 
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT]:
+            player.x += PLAYER_VEL
 
         draw(player)
     pygame.quit()
